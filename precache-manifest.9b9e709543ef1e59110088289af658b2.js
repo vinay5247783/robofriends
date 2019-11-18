7d05e62bb3a1de7284f43e86351d41fd0c6ee114
vinay5247783 <53293059+vinay5247783@users.noreply.github.com>
2019-11-18 15:45:10 +0530

    Updates

---
+++ precache-manifest.9b9e709543ef1e59110088289af658b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531201bf2c3b9c0db905f41a50789aa3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e07f90d3e04ae8708629",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d26e9d5cc50551109380",
+    "url": "/robofriends/static/css/main.0f54a8b4.chunk.css"
+  },
+  {
+    "revision": "e07f90d3e04ae8708629",
+    "url": "/robofriends/static/js/2.7514f082.chunk.js"
+  },
+  {
+    "revision": "d26e9d5cc50551109380",
+    "url": "/robofriends/static/js/main.3101c966.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
