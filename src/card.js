77259f82c15d255d0f4a8fb23cb384ec4149474d
vinay5247783 <53293059+vinay5247783@users.noreply.github.com>
2019-11-18 11:05:25 +0530

    first commit to my robofriends

---
+++ src/card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Card = (props) =>{
+    const {name, email, id} = props;
+	return (
+		<div className = 'bg-light-green tc dib br3 pa3 ma2 grow bw2 shadow-5'>
+		<img alt='robots'   src={`https://robohash.org/${id}?200x200`}/>
+			<div>
+			<h2>{name}</h2>
+
+			<p>{email}</p>
+			
+			</div>
+		</div>
+
+		)
+	}
+
+
+export default Card;
